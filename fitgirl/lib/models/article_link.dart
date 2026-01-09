e68e73866e5d4930c4c34512200ca077d7a8a897
prshhnt <prashant786056@gmail.com>
2026-01-09 23:44:34 +0530

    Add poster URL support to article model for rich previews

--- fitgirl/lib/models/article_link.dart
+++ fitgirl/lib/models/article_link.dart
@@ -1,12 +1,14 @@
-/// Represents a search result article from Fitgirl Repacks
+/// Represents a search result article from FitGirl Repacks
 /// Maps to ArticleLink model from Python backend
 class ArticleLink {
   final String title;
   final String url;
+  final String? posterUrl;
 
   ArticleLink({
     required this.title,
     required this.url,
+    this.posterUrl,
   });
 
   /// Create ArticleLink from JSON response
@@ -14,6 +16,7 @@ class ArticleLink {
     return ArticleLink(
       title: json['title'] as String,
       url: json['url'] as String,
+      posterUrl: json['poster_url'] as String?,
     );
   }
 
@@ -22,6 +25,7 @@ class ArticleLink {
     return {
       'title': title,
       'url': url,
+      'poster_url': posterUrl,
     };
   }
 }
